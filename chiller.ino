e48c52e09ea89219082ed66d122e2decacad840b
Torsten Martinsen <torsten@bullestock.net>
2015-09-08 22:51:46 +0200

    Show temperature, flow, and status.

--- chiller.ino
+++ chiller.ino
@@ -3,13 +3,19 @@
 #include <TM1637Display.h>
 
 // Module connection pins (Digital Pins)
-#define CLK1 2
-#define DIO1 3
-#define CLK2 4
-#define DIO2 5
-#define CLK3 6
-#define DIO3 7
+#define CLK1 4
+#define DIO1 5
+#define CLK2 6
+#define DIO2 7
+#define CLK3 8
+#define DIO3 9
 #define TEMP 10
+#define FLOW 2
+#define RELAY 11
+#define LED 13
+
+#define LOW_TEMP    20
+#define HIGH_TEMP   25
 
 // The amount of time (in milliseconds) between tests
 #define TEST_DELAY   30
@@ -19,17 +25,70 @@ TM1637Display dispFlow(CLK2, DIO2);
 TM1637Display dispStatus(CLK3, DIO3);
 OneWire ds(TEMP);
 
+unsigned long cloopTime;
+
+const uint8_t SEG_OK[] =
+{
+	SEG_A | SEG_C | SEG_D | SEG_E | SEG_F | SEG_G,   // G
+	SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // O
+	SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // O
+	SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F    // D
+};
+
+const uint8_t SEG_COOL[] =
+{
+	SEG_A | SEG_D | SEG_E | SEG_F,                   // C
+	SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // O
+	SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // O
+	SEG_D | SEG_E | SEG_F                            // L
+};
+
+const uint8_t SEG_TEMP[] =
+{
+	SEG_B | SEG_C | SEG_E | SEG_F | SEG_G,           // H
+	SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // O
+	SEG_D | SEG_E | SEG_F | SEG_G,                   // t
+	0                                                // <blank>
+};
+
+const uint8_t SEG_FLOW[] =
+{
+	SEG_A | SEG_E | SEG_F | SEG_G,                   // F
+	SEG_D | SEG_E | SEG_F,                           // L
+	SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // O
+	SEG_B | SEG_D | SEG_F                            // W [sic]
+};
+
+const uint8_t SEG_BARS[] =
+{
+	SEG_G,  // -
+	SEG_G,  // -
+	SEG_G,  // -
+	SEG_G   // -
+};
+
 void setup()
 {
-  dispTemp.setBrightness(0x0f);
-  dispFlow.setBrightness(0x0f);
-  dispStatus.setBrightness(0x0f);
-  uint8_t off[] = { 0, 0, 0, 0 };
-  dispTemp.setSegments(off);
-  dispFlow.setSegments(off);
-  dispStatus.setSegments(off);
+    // Set up interrupt for flow measurement
+    
+    pinMode(FLOW, INPUT);
+    attachInterrupt(0, flow, RISING);
+    sei();
+    cloopTime = millis();
 
-  Serial.begin(9600);
+    // Initialize displays
+    
+    dispTemp.setBrightness(0x0f);
+    dispFlow.setBrightness(0x0f);
+    dispStatus.setBrightness(0x0f);
+    uint8_t off[] = { 0, 0, 0, 0 };
+    dispTemp.setSegments(off);
+    dispFlow.setSegments(off);
+    dispStatus.setSegments(off);
+
+    // Switch relay off
+
+    digitalWrite(RELAY, 1);
 }
 
 void showNumberDecDot(TM1637Display& disp, int num, bool leading_zero, uint8_t length = 4, uint8_t pos = 0, int decimal_dot_place = 1)
@@ -69,47 +128,129 @@ void showNumberDecDot(TM1637Display& disp, int num, bool leading_zero, uint8_t l
     disp.setSegments(digits + (4 - length), length, pos);
 }
 
+volatile int flowFrequency = 0;  // Measures flow meter pulses
+
+// Interrupt function
+void flow()
+{ 
+    ++flowFrequency;
+} 
+
+int on = 0;
+
+bool isHot = false;
+bool noFlow = false;
+
+int loopCount = 0;
+bool showTempError = false;
+
 void loop()
 {
 #if 0
-  int k;
+    int k;
   
-  while(1)
-    for (k = 0; k < 10000; ++k)
-    {
-      dispTemp.showNumberDec(k, false);
-      dispFlow.showNumberDec((k+1000)%10000, false);
-      dispStatus.showNumberDec((k+2000)%10000, false);
-      delay(TEST_DELAY);
-    }
+    while(1)
+        for (k = 0; k < 10000; ++k)
+        {
+            dispTemp.showNumberDec(k, false);
+            dispFlow.showNumberDec((k+1000)%10000, false);
+            dispStatus.showNumberDec((k+2000)%10000, false);
+            delay(TEST_DELAY);
+        }
 #endif
-  byte i;
-  byte present = 0;
-  byte data[12];
-  byte addr[8] = { 0x28, 0xFF, 0xFD, 0x30, 0x64, 0x14, 0x03, 0x44 };
 
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44, 1);         // start conversion, with parasite power on at the end
+    //
+    //-- Read temperature
+    //
+    
+    const byte addr[8] = { 0x28, 0xFF, 0xFD, 0x30, 0x64, 0x14, 0x03, 0x44 };
 
-  delay(1000);     // maybe 750ms is enough, maybe not
-  // we might do a ds.depower() here, but the reset will take care of it.
+    ds.reset();
+    ds.select(addr);
+    ds.write(0x44, 1);         // start conversion, with parasite power on at the end
 
-  present = ds.reset();
-  ds.select(addr);    
-  ds.write(0xBE);         // Read Scratchpad
+    delay(1000);     // maybe 750ms is enough, maybe not
 
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
-    data[i] = ds.read();
-  }
+    ds.reset();
+    ds.select(addr);    
+    ds.write(0xBE);         // Read Scratchpad
+
+    byte data[9];
+    for (int i = 0; i < 9; i++)
+    {
+        // Read 9 bytes
+        data[i] = ds.read();
+    }
   
-  int temp = (data[1] << 8) + data[0];
-  int signBit = temp & 0x8000;
-  if (signBit) // negative
-  {
-    temp = (temp ^ 0xFFFF) + 1; // 2's comp
-  }
-  temp = 6*temp + temp/4;    // multiply by (100 * 0.0625) or 6.25
+    int temp = (data[1] << 8) + data[0];
+    const int signBit = temp & 0x8000;
+    if (signBit) // negative
+    {
+        temp = (temp ^ 0xFFFF) + 1; // 2's comp
+    }
+    temp = 6*temp + temp/4;    // multiply by (100 * 0.0625) or 6.25
 
-  showNumberDecDot(dispTemp, temp, false);
+    showNumberDecDot(dispTemp, temp, false);
+
+    //
+    //-- Read flow (updated every second)
+    //
+    
+    const unsigned long currentTime = millis();
+    if (currentTime >= (cloopTime + 1000))
+    {
+        const unsigned long deltaTime = currentTime-cloopTime;
+        cloopTime = currentTime;
+        // Pulse frequency (Hz) = 7.5Q, Q is flow rate in L/min. (Results in +/- 3% range)
+        const unsigned int litersPerHour = (flowFrequency*60000.0/deltaTime / 7.5);
+        flowFrequency = 0;                   // Reset Counter
+        dispFlow.showNumberDec(litersPerHour);
+
+        digitalWrite(LED, on);
+        on = !on;
+    }
+
+    //
+    //-- Switch compressor on/off
+    //
+    //digitalWrite(RELAY, on);
+
+    // Simulate
+    ++loopCount;
+    if (loopCount > 30)
+    {
+        isHot = true;
+    }
+    if (loopCount > 60)
+    {
+        noFlow = true;
+    }
+    
+    //
+    //-- Show status
+    //
+    if (loopCount % 2)
+    {
+        // Show error state
+        if (isHot || noFlow)
+            showTempError = !showTempError;
+        if (isHot &&
+            (!noFlow || showTempError))
+        {
+            dispStatus.setSegments(SEG_TEMP);
+        }
+        if (noFlow &&
+            (!isHot || !showTempError))
+        {
+            dispStatus.setSegments(SEG_FLOW);
+        }
+    }
+    else
+    {
+        // Show OK or error bars
+        if (isHot || noFlow)
+            dispStatus.setSegments(SEG_BARS);
+        else
+            dispStatus.setSegments(SEG_COOL);
+    }
 }
