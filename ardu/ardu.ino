0c1edfbed8406f491bf9244f2abfdaa2798d1849
Torsten Martinsen <torsten@bullestock.net>
2017-03-31 23:07:32 +0200

    Fix bugs.

--- ardu/ardu.ino
+++ ardu/ardu.ino
@@ -2,6 +2,8 @@
 #include <LiquidCrystal.h>
 #include <OneWire.h>
 
+#define USE_BUZZER  1
+#define SERIAL_DBG  1
 
 const int RELAY_PIN = 12;
 const int FLOW_SENSOR_PIN = 2;  // Must have interrupt support
@@ -20,6 +22,8 @@ const int HOT_TEMP = 30;
 // Halt operation if sensor reports less than this temperature
 const int MIN_ACCEPTABLE_TEMP = 10;
 const int MAX_ACCEPTABLE_TEMP = 50;
+// Sensor reports temperature in hundredths of degrees Celsius
+const int TEMP_SCALE_FACTOR = 100;
 
 // Minimum flow (l/h)
 const int MIN_FLOW = 50;
@@ -52,12 +56,33 @@ byte degrees[8] =
   0b00000,
 };
 
+String format_temp(int temp)
+{
+    char buf[9];
+    // Custom character 1 is a degrees symbol
+    sprintf(buf, "%3d.%d %cC", temp/100, ((temp % 100)+5)/10, 1);
+    return buf;
+}
+
+void print_temp(int line, int temp)
+{
+    lcd.setCursor(11, line);
+    lcd.print(F("     "));
+    lcd.setCursor(11, line);
+    lcd.print(format_temp(temp));
+}
+
 void fatal_error(const char* msg)
 {
+#if SERIAL_DBG
+    Serial.print("FATAL ERROR: ");
+    Serial.println(msg);
+#endif
     lcd.setCursor((20-strlen(msg))/2, 1);
     lcd.print(msg);
     while (1)
     {
+#if USE_BUZZER
         digitalWrite(BUZZER_PIN, 1);
         delay(40);
         digitalWrite(BUZZER_PIN, 0);
@@ -65,6 +90,7 @@ void fatal_error(const char* msg)
         digitalWrite(BUZZER_PIN, 1);
         delay(160);
         digitalWrite(BUZZER_PIN, 0);
+#endif
         delay(320);
     }
 }
@@ -74,12 +100,51 @@ void fatal_error(const String& s)
     fatal_error(s.c_str());
 }
 
+// Get temperature in hundredths of degrees Celsius
+int read_temp(int index)
+{
+    temp_sensors[index]->reset();
+    temp_sensors[index]->select(temp_sensor_addresses[index]);
+    temp_sensors[index]->write(0x44, 1); // start conversion
+
+    delay(750);
+
+    temp_sensors[index]->reset();
+    temp_sensors[index]->select(temp_sensor_addresses[index]);    
+    temp_sensors[index]->write(0xBE);    // Read scratchpad
+
+    byte data[9];
+    for (int i = 0; i < 9; i++)
+        data[i] = temp_sensors[index]->read();
+  
+    // temp holds temperature in degrees Celsius times 100
+    int temp = (data[1] << 8) + data[0];
+    if (temp & 0x8000) // negative
+        temp = (temp ^ 0xFFFF) + 1; // 2's comp
+    temp = 6*temp + temp/4;    // multiply by (100 * 0.0625) or 6.25
+
+#if SERIAL_DBG
+    Serial.print("Temp ");
+    Serial.print(index+1);
+    Serial.print(": ");
+    Serial.println(temp);
+#endif
+
+    return temp;
+}
+
 bool init_temp_sensor(int index)
 {
     for (int attempt = 0; attempt < 5; ++attempt)
     {
+#if SERIAL_DBG
+        Serial.print("Init temp sensor ");
+        Serial.print(index+1);
+        Serial.print(", attempt ");
+        Serial.println(attempt+1);
+#endif
         // Determine temperature sensor address
-
+        
         if (!temp_sensors[index]->search(temp_sensor_addresses[index]))
         {
             fatal_error("No sensor found");
@@ -98,9 +163,9 @@ bool init_temp_sensor(int index)
             Serial.print(temp_sensor_addresses[index][i], HEX);
         Serial.println();
 #endif
-        const double temp = read_temp(index);
-        if ((temp >= MIN_ACCEPTABLE_TEMP) &&
-            (temp <= MAX_ACCEPTABLE_TEMP))
+        const auto temp = read_temp(index);
+        if ((temp >= MIN_ACCEPTABLE_TEMP*TEMP_SCALE_FACTOR) &&
+            (temp <= MAX_ACCEPTABLE_TEMP*TEMP_SCALE_FACTOR))
         {
 #if SERIAL_DBG
             Serial.print("Accepting temperature: ");
@@ -108,38 +173,12 @@ bool init_temp_sensor(int index)
 #endif
             return true;
         }
-        String s = "Bad temperature: ";
+        String s = "Bad temp: ";
         s += format_temp(temp);
         fatal_error(s);
     }
     // Failed to initialize temp sensor properly
     return false;
-}
-
-// Get temperature in hundredths of degrees Celsius
-int read_temp(int index)
-{
-    temp_sensors[index]->reset();
-    temp_sensors[index]->select(temp_sensor_addresses[index]);
-    temp_sensors[index]->write(0x44, 1);         // start conversion, with parasite power on at the end
-
-    delay(1000);     // maybe 750ms is enough, maybe not
-
-    temp_sensors[index]->reset();
-    temp_sensors[index]->select(temp_sensor_addresses[index]);    
-    temp_sensors[index]->write(0xBE);         // Read Scratchpad
-
-    byte data[9];
-    for (int i = 0; i < 9; i++)
-        data[i] = temp_sensors[index]->read();
-  
-    // temp holds temperature in degrees Celsius times 100
-    int temp = (data[1] << 8) + data[0];
-    if (temp & 0x8000) // negative
-        temp = (temp ^ 0xFFFF) + 1; // 2's comp
-    temp = 6*temp + temp/4;    // multiply by (100 * 0.0625) or 6.25
-
-    return temp;
 }
 
 volatile int flowFrequency = 0;  // Measures flow meter pulses
@@ -156,7 +195,10 @@ double temp_readings[2][TEMP_AVERAGES];
 
 void setup() 
 {
-    lcd.createChar(0, degrees);
+    Serial.begin(57600);
+    Serial.println("Chiller v2");
+    
+    lcd.createChar(1, degrees);
     lcd.begin(20, 4);
     lcd.setCursor(4, 1);
     lcd.print(F("Initializing"));
@@ -173,18 +215,25 @@ void setup()
     if (!init_temp_sensor(0))
         fatal_error("SENSOR ERROR:");
 
-    digitalWrite(BUZZER_PIN, true);
     for (int i = 0; i < TEMP_AVERAGES; ++i)
+    {
+        digitalWrite(BUZZER_PIN, true);
+        delay(10);
+        digitalWrite(BUZZER_PIN, false);
         temp_readings[0][i] = read_temp(0);
-    digitalWrite(BUZZER_PIN, false);
+    }
 
-    lcd.setCursor(16, 3);
+    lcd.setCursor(15, 2);
     lcd.print(F("2"));
     if (!init_temp_sensor(1))
         fatal_error("SENSOR ERROR:");
-    digitalWrite(BUZZER_PIN, true);
     for (int i = 0; i < TEMP_AVERAGES; ++i)
+    {
+        digitalWrite(BUZZER_PIN, true);
+        delay(15);
+        digitalWrite(BUZZER_PIN, false);
         temp_readings[1][i] = read_temp(1);
+    }
     digitalWrite(BUZZER_PIN, false);
     
     // Set up interrupt for flow measurement
@@ -196,34 +245,14 @@ void setup()
     lcd.clear();
 }
 
-String format_temp(int temp)
-{
-    char buf[5];
-    sprintf(buf, "%3d", temp/10);
-    String s = buf;
-    s += F(".");
-    sprintf(buf, "%d", temp % 10);
-    s += buf;
-    // Custom character 0 is a degrees symbol
-    s += F(" \x00C");
-    return s;
-}
-
-void print_temp(int line, int temp)
-{
-    lcd.setCursor(11, line);
-    lcd.print(F("     "));
-    lcd.setCursor(11, line);
-    lcd.print(format_temp(temp));
-}
-
 void loop() 
 {
     for (int j = 0; j < 2; ++j)
     {
         int temp = read_temp(j);
         lcd.setCursor(0, j);
-        lcd.print(F("Temp 1"));
+        lcd.print(F("Temp "));
+        lcd.print(j+1);
         print_temp(j, temp);
     }
     
@@ -263,6 +292,7 @@ void loop()
     lcd.print(state);
 
     digitalWrite(RELAY_PIN, random(0, 2));
+    digitalWrite(COMPRESSOR_PIN, random(0, 2));
 
-    delay(1000);
+    delay(5000);
 }
