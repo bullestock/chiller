b353d9bcbfbc84aaca6dd2d0011c1e9499c58931
Torsten Martinsen <torsten@bullestock.net>
2017-05-12 23:50:17 +0200

    Add fan control. Drive SSR from 12V.

--- flowsim/flowsim.ino
+++ flowsim/flowsim.ino
@@ -3,12 +3,16 @@ const int SENSOR_PIN = 2;
 void setup()
 {
     pinMode(SENSOR_PIN, OUTPUT);
+    Serial.begin(115200);
+    Serial.println("Flow sensor simulator ready");
 }
 
 int del = 100;
 
 void loop()
 {
+    Serial.print("Delay: ");
+    Serial.println(del);
     for (int i = 0; i < 10000/del; ++i)
     {
         digitalWrite(SENSOR_PIN, 0);
