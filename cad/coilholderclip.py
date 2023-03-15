147d15f11dadd833f3c868c3606a78a089998d7c
Torsten Martinsen <torsten@bullestock.net>
2023-03-15 18:12:28 +0100

    Clip for holding together the two coil holders.

---
+++ cad/coilholderclip.py
@@ -0,0 +1,16 @@
+import cadquery as cq
+
+d = 6
+eps = 0.2
+hh = 2*20+eps
+hw = 9+eps
+th = 8
+
+res = (cq.Workplane("XY")
+       .box(hh+2*th, hw+2*th, d)
+       .workplane()
+       .rect(hh, hw)
+       .cutThruAll()
+      )
+
+show_object(res)
