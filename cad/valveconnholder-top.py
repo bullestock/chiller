48c03c65e9482a9a24549ede800ab1933daffe83
Torsten Martinsen <torsten@bullestock.net>
2023-04-23 21:10:21 +0200

    Retaining clip.

---
+++ cad/valveconnholder-top.py
@@ -0,0 +1,56 @@
+import cadquery as cq
+
+d1 = 16
+d2 = 25
+d3 = 5
+w1 = 12.5
+w2 = 20
+w3 = 40
+th1 = 2
+h1 = 25.5
+h2 = 40
+
+# Outer shape
+res = (cq.Workplane("XY")
+       .tag("bot")
+       .rect(w3, h2)
+       .extrude(d3)
+       .edges("|Z")
+       .fillet(5)
+       .edges(">Z")
+       .fillet(1)
+       )
+
+cutout = (cq.Workplane("XY")
+          .tag("cbot")
+          # Main cavity
+          .rect(h1 + 2, h1)
+          .extrude(d3)
+          .edges("|Z")
+          .fillet(2)
+          .workplaneFromTagged("cbot")
+          .rect(12, h2)
+          .workplane(d3)
+          .rect(11, h2)
+          .loft()
+          )
+
+res = res - cutout
+
+# Screw holes
+
+csd = 3.2
+
+res = (res
+       .workplaneFromTagged("bot")
+       .rarray((w2+w3)/2, (h1+h2)/2, 2, 2)
+       .circle(csd/2)
+       .cutThruAll()
+       )
+
+res = (res
+       .faces(">X").workplane(-h1)
+       .split(keepTop=True)
+       )
+show_object(res)
+#show_object(cutout)
