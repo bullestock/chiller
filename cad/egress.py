0f933b91b9fd1a133aae7cf0275cb2a4f429634c
Torsten Martinsen <torsten@bullestock.net>
2023-05-30 18:34:36 +0200

    Complete.

--- cad/egress.py
+++ cad/egress.py
@@ -5,6 +5,7 @@ tube_r = 9/2 # tube inner radius
 d1 = 20 # height of vertical tube
 r1 = 25 # radius of first bend
 a1 = 89 # angle of first bend in degrees
+d2 = 1 # length of horizontal tube
 wth = 1.5 # tube wall thickness
 plate_w = 28
 plate_th = 3
@@ -16,18 +17,15 @@ ap1 = (r1 - math.cos(math.radians(a1/2))*r1, d1 + math.sin(math.radians(a1/2))*r
 ap2 = (r1 - math.cos(math.radians(a1))*r1, d1 + math.sin(math.radians(a1))*r1)
 
 # create path
-path = cq.Workplane("XZ" ).vLine(d1).threePointArc(ap1, ap2)
+path = cq.Workplane("XZ" ).vLine(d1).threePointArc(ap1, ap2).polarLine(d2, 90 - a1)
 
 # create tube by sweeping along path
 tube = cq.Workplane("XY").circle(tube_r).sweep(path).faces(">Z or <Z").shell(wth)
 
-show_object(tube)
-
-#plate = (cq.Workplane("ZY")
-plate = (tube#.faces(">X")
+plate = (tube
          .center(0, 0)
          .workplane()
-         .transformed(rotate=(0, 270, 0), offset=(r1, 0, r1+d1))
+         .transformed(rotate=(0, 270, 0), offset=(r1+d2, 0, r1+d1))
          .tag("pbot")
          .rect(plate_w, plate_w)
          .extrude(plate_th)
@@ -45,4 +43,13 @@ plate = (tube#.faces(">X")
          .circle(screw_r)
          .cutThruAll()
          )
-show_object(plate)
+
+# ribs on tube
+ring = cq.Workplane("YZ").moveTo(tube_r+wth*0.7).circle(1).revolve()
+
+res = (plate
+       .union(ring.translate((0, 0, 5)), clean=False)
+       .union(ring.translate((0, 0, 10)), clean=False)
+       .union(ring.translate((0, 0, 15)), clean=False)
+       )
+show_object(res)
