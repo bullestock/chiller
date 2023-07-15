2657473974b40992f3d83c75a5ae7b13ad4466c5
Torsten Martinsen <torsten@bullestock.net>
2023-07-15 12:28:30 +0200

    Use loft().

--- cad/topmoulding.py
+++ cad/topmoulding.py
@@ -8,10 +8,14 @@ d2 = 3
 # chamfer amount
 ch = 1.5
 # slot width
-sw = 3.4 # was 3.2
+sw = 3.25
 # slot depth
 sd = 2
+# knob/hole dimension
+kn_w = 2
+kn_l = 4
 
+# basic profile
 result = (cq.Workplane("XY")
           .tag("o")
           # profile
@@ -21,8 +25,63 @@ result = (cq.Workplane("XY")
           .hLine(-d1)
           .close()
           .extrude(length)
-          .faces(">Z or <Z")
-          .chamfer(ch)
+          )
+
+# lower end
+result = (result
+          .workplaneFromTagged("o")
+          .hLine(d1 + d2)
+          .vLine(d1)
+          .lineTo(d1, d1 + d2)
+          .hLine(-d1)
+          .close()
+          .workplaneFromTagged("o")
+          .transformed(offset=(0, 0, -ch))
+          .hLine(d1 + d2 - ch)
+          .vLine(d1 - ch)
+          .lineTo(d1 - ch, d1 + d2 - ch)
+          .hLine(-(d1 - ch))
+          .close()
+          .loft()
+          )
+
+# upper end
+result = (result
+          .workplaneFromTagged("o")
+          .transformed(offset=(0, 0, length))
+          .hLine(d1 + d2)
+          .vLine(d1)
+          .lineTo(d1, d1 + d2)
+          .hLine(-d1)
+          .close()
+          .workplaneFromTagged("o")
+          .transformed(offset=(0, 0, length + ch))
+          .hLine(d1 + d2 - ch)
+          .vLine(d1 - ch)
+          .lineTo(d1 - ch, d1 + d2 - ch)
+          .hLine(-(d1 - ch))
+          .close()
+          .loft()
+          )
+
+# knob
+kn_offset = 0.3
+result = (result
+          .workplaneFromTagged("o")
+          .transformed(offset=((d1 + d2)/2 - kn_offset , (d1 + d2)/2 - kn_offset , length + ch))
+          .rect(kn_w, kn_w)
+          .extrude(kn_l)
+          )
+
+# hole
+result = (result
+          .workplaneFromTagged("o")
+          .transformed(offset=((d1 + d2)/2 - kn_offset , (d1 + d2)/2 - kn_offset , -ch))
+          .rect(kn_w + 0.1, kn_w + 0.1)
+          .cutBlind(kn_l + 1)
+          )
+
+result = (result
           # vertical slot
           .workplaneFromTagged("o")
           .transformed(offset=((d1 + d2)/2, sd/2, 0))
@@ -33,7 +92,7 @@ result = (cq.Workplane("XY")
           .transformed(offset=(sd/2, (d1 + d2)/2, 0))
           .rect(sd, sw)
           .cutThruAll()
-)
+          )
 
 show_object(result)
 
